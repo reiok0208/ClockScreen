c688e64d80f4d3b865851d9d6e28d83b2d02fecc
reiok0208 <62207092+reiok0208@users.noreply.github.com>
2022-09-29 16:08:15 +0900

    first_commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg2="HelloWorld"/> -->
+  <Clock />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/NewPage.vue'
+import Clock from './components/ClockScreen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Clock,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  html,
+  body {
+    height: 100%;
+    margin: unset;
+  }
+  #app {
+    height: 100%;
+  }
+  </style>
